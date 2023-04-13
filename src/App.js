9366fd706f5e5d51b5bb3206030ab3ac7f2d8194
NEWUSER <chasevriend84@gmail.com>
2023-04-13 12:00:02 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TinderCards from './TinderCards';
 import Chats from './Chats';
+import ChatScreen from './ChatScreen';
 import './App.css';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<TinderCards />} />
           <Route path="/chats" element={<Chats />} />
+          <Route path='/chats/:person' element={<ChatScreen />} />
         </Routes>
       </>
     </div>
